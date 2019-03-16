9650ee2b77d7386c239c1873c3911fdfcc25eb14
Shi-Tang Liu <rbufghj9713@gmail.com>
2019-03-16 22:29:27 +0800

    hw2_exp_01 order : 1 0.5  ite=2000

--- hw2/logestic.py
+++ hw2/logestic.py
@@ -12,7 +12,7 @@ prediction_file = sys.argv[6]
 
 # read in parsed data for training #######################################################################
 data_in = np.genfromtxt(train_feature,delimiter = ',',skip_header=1) 
-data_in = np.concatenate((data_in,data_in**2,data_in**3,data_in**4,np.log(data_in + 1e-8),data_in*np.log(data_in + 1e-8)),axis=1)
+data_in = np.concatenate((data_in,data_in**2,np.log(data_in+1e-8)),axis=1)
 label_in = np.genfromtxt(train_label,delimiter = ',' , skip_header=1)
 # print (np.shape(data_in),np.shape(label_in))
 
@@ -35,7 +35,7 @@ num_fea = len(data_in[0])
 num_data = data_in.shape[0]
 data_tran = data_in.transpose()
 lr = 0.1
-iteration = 6000
+iteration = 3000
 w = np.zeros(num_fea)
 # print(len(data_in[0]))
 b = 0.1
@@ -45,6 +45,7 @@ s_b = 0.0
 loss_test = []
 loss_train = []
 
+lamda = 0
 
 for it in range(iteration) :
   # TO-DO
@@ -55,11 +56,11 @@ for it in range(iteration) :
   s_w += wg**2
   s_b += bg**2
   
-  w -= lr*wg/np.sqrt(s_w)
+  w -= lr*wg/np.sqrt(s_w) + lamda*w
   b -= lr*bg/np.sqrt(s_b)
 
 
-  if it%100 == 0 : 
+  if it%10 == 0 : 
     t_pre = (pre >= 0.5)
     t_dif = (t_pre == label_in ) 
     t_dif = t_dif.astype('int')
@@ -81,13 +82,13 @@ plt.show()
 
 
 test_data = np.genfromtxt(test_feature,delimiter = ',',skip_header=1)
-test_data = np.concatenate((test_data,test_data**2,test_data**3,test_data**4,np.log(test_data + 1e-8),test_data*np.log(test_data + 1e-8)),axis=1)
+test_data = np.concatenate((test_data,test_data**2,np.log(test_data + 1e-8)),axis=1)
 
 # normalize data #######################################################################################
 test_std = np.std(test_data,axis=0)
 test_std += 0.00000000000000000001
 test_mean = np.mean(test_data,axis=0)
-test_data = np.divide(np.subtract(test_data,test_mean),test_std)
+test_data = np.divide(np.subtract(test_data,test_mean),std)
 
 
 # print(np.shape(test_data))
