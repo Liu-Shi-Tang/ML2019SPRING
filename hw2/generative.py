9cd2a7a41578eab2c74da4f7c7632e2c9baea92e
Shi-Tang Liu <rbufghj9713@gmail.com>
2019-03-16 18:55:27 +0800

    generative can pass simple best line

--- hw2/generative.py
+++ hw2/generative.py
@@ -12,13 +12,8 @@ prediction_file = sys.argv[6]
 
 # read in parsed data for training #########################################################################
 data_in = np.genfromtxt(train_feature,delimiter = ',',skip_header=1) 
-data_in = np.concatenate((data_in,data_in**2,data_in**3,data_in**4,np.log(data_in + 1e-8),data_in*np.log(data_in + 1e-8)),axis=1)
 label_in = np.genfromtxt(train_label,delimiter = ',' , skip_header=1)
 # print (np.shape(data_in),np.shape(label_in))
-
-# define sigmoid function for prediction ###################################################################
-def mysigmoid(z) :
-  return np.clip(1 / ( 1 + np.exp(-1*z)),0.00000000000000001,0.999999999999999)
 
 # Find Mu for two class ####################################################################################
 
@@ -31,12 +26,17 @@ num_of_class_low  = 0
 mean_high = np.zeros((1,size_of_feature))
 mean_low  = np.zeros((1,size_of_feature))
 
+# We should divide data into two group
+class_high = []
+class_low  = []
 
 for i in range(len(data_in)) :
   if label_in[i] == 1 :
+    class_high.append(data_in[i])
     mean_high += data_in[i] 
     num_of_class_high += 1
   else :
+    class_low.append(data_in[i])
     mean_low += data_in[i]
     num_of_class_low += 1
 
@@ -46,5 +46,47 @@ if num_of_class_high > 0 :
 if num_of_class_low > 0 :
   mean_low /= num_of_class_low
 
+
+# start to calculate sigma for same covariance
+
+print(np.transpose((class_high[0] - mean_high)).shape,type((class_high[0] - mean_high)))
+print(np.dot( np.transpose(class_low[0]  - mean_low ) , class_low[0] - mean_low  ).shape)
+
+
+cov_high = np.zeros((size_of_feature,size_of_feature))
+cov_low  = np.zeros((size_of_feature,size_of_feature))
+
+for i in range(num_of_class_high) :
+  cov_high += np.dot( np.transpose(class_high[i]  - mean_high ) , class_high[i] - mean_high  ) / num_of_class_high
+for i in range(num_of_class_low) :
+  cov_low += np.dot( np.transpose(class_low[i]  - mean_low ) , class_low[i] - mean_low  ) / num_of_class_low
+
+
+
+cov = (cov_high*num_of_class_high + cov_low*num_of_class_low ) / (num_of_class_high + num_of_class_low)
+
+print(cov.shape,type(cov))
+cov_inv = np.linalg.inv(cov)
+
+w = np.dot(mean_high-mean_low,cov_inv)
+b = (-0.5) * np.dot(np.dot(mean_high,cov_inv) , np.transpose(mean_high) ) + 0.5 * np.dot( np.dot(mean_low,cov_inv), np.transpose(mean_low) ) + np.log(float(num_of_class_high)/num_of_class_low)
+
+
+# read testing data
+test_data = np.genfromtxt(test_feature,delimiter = ',',skip_header=1)
+
+
+
+z = np.dot(test_data,np.transpose(w)) + b
+ans = 1 / ( 1 + np.exp(-z))
+ans = (ans >= 0.5)
+
+
+out = open(prediction_file,'w')
+# print(len(y_test))
+out.write("id,label\n")
+for i in range(len(ans)) :
+  out.write(str(i+1) + ',' + str(int(ans[i])) + '\n')
+out.close()
 
 
