7eba4df080c3dc9f29b4f2df040718e40dcaf326
lab LST <rbufghj9713@gmail.com>
2019-04-08 14:40:44 +0800

    hw3: tmp

--- hw3/train_both.py
+++ hw3/train_both.py
@@ -176,7 +176,7 @@ es = keras.callbacks.ReduceLROnPlateau(monitor='val_loss',
 # datagen.fit(train_feature)
 history = model.fit_generator(
     datagen.flow(train_feature,train_label,batch_size=128),
-    steps_per_epoch=len(train_feature)/128,
+    steps_per_epoch=len(train_feature)/32,
     epochs=250,
     validation_data=(valid_feature,valid_label),
     callbacks=[mcp,es])
