f04d68f87860710cecd1f144ad98befaf2190daa
lab LST <rbufghj9713@gmail.com>
2019-04-08 08:24:20 +0800

    hw3: add script

---
+++ hw3/runThreeVersion.sh
@@ -0,0 +1,10 @@
+
+
+python ./train_no.py ../../ML_data/hw3/train.csv ../../ML_data/hw3/test.csv
+python ./train_nor_only.py ../../ML_data/hw3/train.csv ../../ML_data/hw3/test.csv
+python ./train_both.py ../../ML_data/hw3/train.csv ../../ML_data/hw3/test.csv
+
+
+
+
+
