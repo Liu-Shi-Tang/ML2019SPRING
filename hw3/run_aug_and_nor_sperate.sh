b39f4b69eb26a0fa4b3f8e83528b377d2c87db75
lab LST <rbufghj9713@gmail.com>
2019-04-09 10:18:50 +0800

    hw3: clean dir

--- hw3/run_aug_and_nor_sperate.sh
+++
@@ -1,6 +0,0 @@
-
-
-python ./train_aug_only.py ../../ML_data/hw3/train.csv ../../ML_data/hw3/test.csv
-python ./train_nor_only.py ../../ML_data/hw3/train.csv ../../ML_data/hw3/test.csv
-
-
--- hw3/test.py
+++ hw3/test.py
@@ -27,17 +27,31 @@ num_test = len(test_in)
 for i in range(num_test) :
   test_in[i,0] = test_in[i,0].split(',')[1]
 
-
 # reshaping testing data
 test_in = np.reshape(test_in,(num_test,48,48,1))
 test_in = test_in.astype(float)
 test_in = test_in/255
 
-model = load_model( 'mcp-acc-0.67050.h5' )
+model1 = load_model('mcp-best-acc-0.68250.h5')
+model2 = load_model('mcp-both-acc-0.68450.h5')
+mean = np.load('both_mean.npy')
+std = np.load('both_std.npy')
 
-result = model.predict(test_in)
+# without std
+result1 = model1.predict(test_in)
 
-f = open('default.csv','w')
+# using std
+test_in = (test_in - mean) / (std + 1e-20)  
+result2 = model2.predict(test_in)
+
+# combine two result
+result = result1 + result2
+
+# write result
+result_file = sys.argv[2]
+
+
+f = open(result_file,'w')
 f.write("id,label\n")
 for i in range(len(result)) :
   f.write(str(i) + ',' + str(int(np.argmax(result[i]))) + '\n')
