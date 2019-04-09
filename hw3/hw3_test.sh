c9dfbfd15f2fee85e11e1f8bc08d102a9544f019
lab LST <rbufghj9713@gmail.com>
2019-04-09 09:39:39 +0800

    hw3: add test.script

---
+++ hw3/hw3_test.sh
@@ -0,0 +1,9 @@
+
+
+
+wget https://github.com/Liu-Shi-Tang/ML_model/releases/download/hw3/both_mean.npy
+wget https://github.com/Liu-Shi-Tang/ML_model/releases/download/hw3/both_std.npy
+wget https://github.com/Liu-Shi-Tang/ML_model/releases/download/hw3/mcp-best-acc-0.68250.h5
+wget https://github.com/Liu-Shi-Tang/ML_model/releases/download/hw3/mcp-both-acc-0.68450.h5
+python test.py $1 $2
+
