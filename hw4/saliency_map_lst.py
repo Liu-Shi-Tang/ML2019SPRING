61306b825164246aced3886b122bcb3f51cfb5df
lab LST <rbufghj9713@gmail.com>
2019-04-11 01:22:32 +0800

    hw4: update saliency_map_lst.py

--- hw4/saliency_map_lst.py
+++ hw4/saliency_map_lst.py
@@ -63,14 +63,16 @@ def parsingInput(file_name) :
   sal_label = []
   for i in range(7) :
     for j in range(len(feature)) :
-      if ( str(label[j]) == str(i) ) :
+      if ( int(label[j]) == int(i) ) :
         sal_feature.append(feature[j])
         sal_label.append(label[j])
         print(j)
         break ;
-
-  np.save('Y_train_label',sal_label)
-  np.save('X_train',sal_feature)
+  sal_feature = np.array(sal_feature)
+  sal_label = np.array(sal_label)
+  # np.save('Y_train_label',sal_label)
+  # np.save('X_train',sal_feature)
+  return sal_feature,sal_label
 
 class GradientSaliency():
   def __init__(self, model, output_index = 0):
@@ -96,33 +98,16 @@ class GradientSaliency():
     # return the gradient calculated by keras object which was created in constructor
     return gradients
 
-  def get_smoothed_mask(self, input_image, stdev_spread=.2, nsamples=50):
-    # Fix seed for reproducibility
-    np.random.seed(666)
-    # constatnt for tuning noise (Standard Deviation)
-    stdev = stdev_spread * (np.max(input_image) - np.min(input_image))
-    # To get a whole zero np.array with the size of input_image 
-    total_gradients = np.zeros_like(input_image, dtype = np.float64)
-    # To randomly add noise for more smoth
-    for i in range(nsamples):
-        noise = np.random.normal(0, stdev, input_image.shape)
-        x_value_plus_noise = input_image + noise
-        # add noise to original mask
-        total_gradients += self.get_mask(x_value_plus_noise)
-
-    return total_gradients / nsamples
-
 
 # read model
 model_name = 'mcp-best-acc-0.68250.h5'
 model = load_model(model_name)
 # read data (label is not on-hot format)
 dataFile = sys.argv[1]
-# Only need to run first time
-parsingInput(dataFile)
 # loading data from previous 
-valid_feature = np.load('X_train.npy')
-valid_label = np.load('Y_train_label.npy')
+features, labels =   parsingInput(dataFile)
+print(features.shape,labels.shape)
+  
 n_classes = 7
 # which picture we want to generate saliency map for 
 which_picture = [0,1,2,3,4,5,6]
@@ -131,29 +116,25 @@ which_picture = [0,1,2,3,4,5,6]
 # Y_train_label = np.load('Y_train_label.npy')
 
 
-fig, ax = plt.subplots(nrows=n_classes,  ncols=5, figsize = (12, 16))
+fig, ax = plt.subplots(nrows=n_classes,  ncols=3)
 fig.suptitle('SaliencyMap')
 
 
 
 for i in which_picture :
   # get a image
-  img = valid_feature[i]
-  print(valid_label[i])
+  img = features[i]
+  print(labels[i])
   # initialize customized object
-  vanilla = GradientSaliency(model, int(valid_label[i]))
+  vanilla = GradientSaliency(model, int(labels[i]))
   # To get the mask from gradients
   mask = vanilla.get_mask(img)
   filter_mask = (mask > 0.0).reshape((48, 48))
-  smooth_mask = vanilla.get_smoothed_mask(img)
-  filter_smoothed_mask = (smooth_mask > 0.0).reshape((48, 48))
+
 
   ax[i, 0].imshow(img.reshape((48, 48)), cmap = 'gray')
   cax = ax[i, 1].imshow(mask.reshape((48, 48)), cmap = 'jet')
   fig.colorbar(cax, ax = ax[i, 1])
   ax[i, 2].imshow(mask.reshape((48, 48)) * filter_mask, cmap = 'gray')
-  cax = ax[i, 3].imshow(mask.reshape((48, 48)), cmap = 'jet')
-  fig.colorbar(cax, ax = ax[i, 3])
-  ax[i, 4].imshow(smooth_mask.reshape((48, 48)) * filter_smoothed_mask, cmap = 'gray')
 plt.savefig('Saliency_map.png')
 plt.show()
