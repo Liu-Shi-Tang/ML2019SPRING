854ed581ff0516a7a4b9f12bb92e476e7864fa5e
lab LST <rbufghj9713@gmail.com>
2019-05-08 17:24:35 +0800

    hw6: update script

--- hw6/hw6_test.sh
+++ hw6/hw6_test.sh
@@ -9,6 +9,12 @@ testX="392045_data/test_x.csv"
 dictTxtBig="./dict.txt.big/dict.txt.big"
 result="result.csv"
 
+# downdload model
+wget https://github.com/Liu-Shi-Tang/ML_model/releases/download/HW6/best.h5 
+wget https://github.com/Liu-Shi-Tang/ML_model/releases/download/HW6/best2.h5
+wget https://github.com/Liu-Shi-Tang/ML_model/releases/download/HW6/best3.h5
+wget https://github.com/Liu-Shi-Tang/ML_model/releases/download/HW6/word2vec.model
+
 if [ $# -eq 3 ]
 then
   echo "get 3 arg"
