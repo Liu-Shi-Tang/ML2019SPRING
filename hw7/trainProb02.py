912d8c58e2040ec76d0302b3553264e3e4abbb12
lab LST <rbufghj9713@gmail.com>
2019-05-23 23:58:42 +0800

    hw7: modify train.py

--- hw7/trainProb02.py
+++ hw7/trainProb02.py
@@ -11,6 +11,7 @@ from keras.layers.normalization import BatchNormalization
 from sklearn.metrics import mean_absolute_error
 from sklearn.decomposition import PCA
 
+inputDir = sys.argv[1] 
 
 def getModel (shape_in=(32,32,3)) :
   input_img = Input(shape=shape_in)
@@ -45,16 +46,11 @@ def getModel (shape_in=(32,32,3)) :
   return input_img,encoded,decoded,autoencoder
 
 
-inputDir = sys.argv[1] 
-
-# extract file name
-# pngList = [ file for file in os.listdir(inputDir) if file.endswith('.jpg')]
-
 pngList = []
 for i in range(1,40001,1) :
   pngList.append("%06d" %i)
 
-pictures = [ np.asarray(Image.open(inputDir + pic + '.jpg')) for pic in pngList ]
+pictures = [ np.asarray(Image.open( os.path.join(inputDir,(pic + '.jpg'))  )) for pic in pngList ]
 pictures = np.array(pictures)
 
 
@@ -91,19 +87,4 @@ processInputImgs = pca.transform(processInputImgs)
 
 result = KMeans(n_clusters = 2, max_iter=5000, n_init=50 ,verbose = 0 , n_jobs=-1 , random_state=seed).fit(processInputImgs)
 
-# 
-# testFile = sys.argv[2]
-# 
-# test = np.genfromtxt( testFile , delimiter= ',' , dtype=int , skip_header=1 )
-# i1 , i2 = test[:,1] , test[:,2]
 
-
-ans_label = np.load('mylabel.npy')
-
-
-
-count = 0 
-for i in range(len(ans_label)) :
-  if ans_label[i] == result.labels_[i] :
-    count += 1
-print("acc: ",count/len(ans_label))
