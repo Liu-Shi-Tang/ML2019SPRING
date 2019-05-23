f08f2666670f4ce7e0b072b1b6cf7731ada49027
lab LST <rbufghj9713@gmail.com>
2019-05-24 00:59:50 +0800

    hw7 update cluster.sh

--- hw7/cluster.sh
+++ hw7/cluster.sh
@@ -4,4 +4,8 @@
 # $2 : test_case.csv path 
 # $3 : predict file name(containing path)
 
+wget https://github.com/Liu-Shi-Tang/ML_model/releases/download/HW7/best.h5
+wget https://github.com/Liu-Shi-Tang/ML_model/releases/download/HW7/encoder.h5
+
+
 python testProb.py ${1} ${2} ${3}
